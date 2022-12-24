9d775de093268bbd5d13d633b7f10232c20d8573
Evgeniy <freemen0083@gmail.com>
2022-12-24 18:47:33 +0400

    #3

---
+++ Program (3).cs
@@ -0,0 +1,45 @@
+﻿int[,] FillShowArrey(int[,] arr, int rows, int cols, int min, int max)
+{
+    int[,] arrey = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            arrey[i, j] = new Random().Next(min, max + 1);
+            Console.Write("    " + arrey[i, j]);
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+    return arrey;
+}
+void AveregeCols(int[,] farrey)
+{double dif=0;
+    
+    for (int i= 0; i < farrey.GetLength(0); i++)
+    
+    {
+        for (int j = 0; j < farrey.GetLength(1); j++)
+        {
+            Console.Write("    " + farrey[i, j]);
+            dif=(farrey[j, i]+dif)/(farrey.GetLength(1));
+        }
+    Console.WriteLine("  "+dif);
+    }
+
+}
+Console.WriteLine("input rows :  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input cols :  ");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input min :  ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input max :  ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] arr = new int[rows, cols];
+
+int[,] farrey = FillShowArrey(arr, rows, cols, min, max);
+AveregeCols(farrey);
+
