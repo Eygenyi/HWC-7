a6e767caadb1a34881517eeda6d5d2e960f10e0e
Evgeniy <freemen0083@gmail.com>
2022-12-24 18:37:54 +0400

    #1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿void FillShowArrey(double[,] arr, int rows, int cols)
+{
+    double[,] arrey = new double[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            arrey[i, j] = new Random().NextDouble() * 10;
+            Console.Write("    " + Math.Round((arrey[i, j]), 3));
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("input rows :  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input cols :  ");
+int cols = Convert.ToInt32(Console.ReadLine());
+double[,] arr = new double[rows, cols];
+FillShowArrey(arr, rows, cols);
